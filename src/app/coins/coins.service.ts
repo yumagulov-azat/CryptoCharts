2f680d7d8cb4c2259ef23c04ce3504c72947b22f
Азат Юмагулов <jumagulov.azat@gmail.com>
2018-02-15 16:34:22 +0500

    Smart currency formating

--- src/app/coins/coins.service.ts
+++ src/app/coins/coins.service.ts
@@ -1,11 +1,7 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import {Observable} from 'rxjs/observable';
-import {forkJoin} from "rxjs/observable/forkJoin";
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/mergeMap';
 import {CCC} from '../../utilities/ccc-streamer-utilities';
-// console.log(CCC.CURRENT.unpack())
 
 /**
  * Work with cryptocompare API
@@ -24,81 +20,69 @@ export class CoinsService {
    */
   getCoinsList(limit: number = 50, page: number = 0): Observable<any> {
     return Observable.create((observer) => {
-      this.http.get('https://min-api.cryptocompare.com/data/top/totalvol', {
-        params: {
-          limit: limit,
-          page: page,
-          tsym: 'USD'
-        }
-      }).subscribe((res: {Message: string, Data: any}) => {
-        let coinsList: Array<{}> = [];
+      let params = new HttpParams()
+        .set('limit', limit.toString())
+        .set('page', page.toString())
+        .set('tsym', 'USD');
 
-        if (res.Message == 'Success') {
-          res.Data.forEach((item, index) => {
-            let coinInfo       = item.CoinInfo,
-                conversionInfo = item.ConversionInfo,
-                priceInfo      = CCC.CURRENT.unpack(conversionInfo.RAW[0]);
+      this.http.get<any>('https://min-api.cryptocompare.com/data/top/totalvol', { params: params })
+        .subscribe((res: {Message: string, Data: any}) => {
+          let coinsList: Array<{}> = [];
 
-            // console.log(item)
-            coinsList.push({
-              position: page*limit + (index+1),
-              name: coinInfo.Name,
-              fullName: coinInfo.FullName,
-              imageUrl: coinInfo.ImageUrl,
-              price: priceInfo.PRICE,
-              open24Hour: priceInfo.OPEN24HOUR,
-              change24Hour: priceInfo.PRICE - priceInfo.OPEN24HOUR,
-              changePct24Hour: ((priceInfo.PRICE - priceInfo.OPEN24HOUR) / priceInfo.OPEN24HOUR * 100).toFixed(2),
-              coinsMined: conversionInfo.Supply,
-              marketCap: priceInfo.PRICE * conversionInfo.Supply
+          if (res.Message == 'Success') {
+            res.Data.forEach((item, index) => {
+              let coinInfo       = item.CoinInfo,
+                  conversionInfo = item.ConversionInfo,
+                  priceInfo      = CCC.CURRENT.unpack(conversionInfo.RAW[0]);
+
+              // console.log(item)
+              coinsList.push({
+                position: page * limit + (index + 1),
+                name: coinInfo.Name,
+                fullName: coinInfo.FullName,
+                imageUrl: coinInfo.ImageUrl,
+                price: CCC.convertValueToDisplay('$', priceInfo.PRICE),
+                open24Hour: priceInfo.OPEN24HOUR,
+                change24Hour: priceInfo.PRICE - priceInfo.OPEN24HOUR,
+                changePct24Hour: ((priceInfo.PRICE - priceInfo.OPEN24HOUR) / priceInfo.OPEN24HOUR * 100).toFixed(2),
+                coinsMined: conversionInfo.Supply,
+                marketCap: CCC.convertValueToDisplay('$', priceInfo.PRICE * conversionInfo.Supply, 'short')
+              });
             });
-          });
 
-          observer.next(coinsList);
-          observer.complete();
+            observer.next(coinsList);
+            observer.complete();
 
-        } else {
-          observer.error('API error');
-        }
-      });
+          } else {
+            observer.error('API error');
+          }
+        });
 
     });
   }
 
 
-  // getCoinsList(limit: number = 50, page: number = 0): Observable<any> {
-  //   return this.http.get('https://min-api.cryptocompare.com/data/top/totalvol', {
-  //       params: {
-  //         limit: limit,
-  //         page: page,
-  //         tsym: 'USD'
-  //       }
-  //     })
-  // }
-
   /**
    * Get coin data
    */
   getCoinFullData(coinName: string): Observable<any> {
-    return this.http.get('https://www.cryptocompare.com/api/data/coinsnapshot/', {
-      params: {
-        fsym: coinName,
-        tsym: 'USD'
-      }
-    })
+    let params = new HttpParams()
+      .set('fsym', coinName)
+      .set('tsym', 'USD');
+
+    return this.http.get('https://www.cryptocompare.com/api/data/coinsnapshot/', { params: params })
   }
 
   /**
    * Get coin history by days
    */
   getCoinHistoryByDays(coinName: string, limit: number = 365): Observable<any> {
-    return this.http.get('https://min-api.cryptocompare.com/data/histoday', {
-      params: {
-        fsym: coinName,
-        tsym: 'USD',
-        limit: limit
-      }
-    })
+    let params = new HttpParams()
+      .set('limit', limit.toString())
+      .set('fsym', coinName)
+      .set('tsym', 'USD');
+
+    return this.http.get('https://min-api.cryptocompare.com/data/histoday', { params: params })
   }
 
 }
